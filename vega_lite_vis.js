eb50666a2a9555c256b440196ff9ca56947c31c7
xuenhan <xshe0033@student.monash.edu>
2025-10-02 19:36:35 +0800

    Combined two visualisations

---
+++ vega_lite_vis.js
@@ -0,0 +1,7 @@
+var vg_1 = "mean_household_income.vg.json";
+vegaEmbed("#chloropleth_map", vg_1).then(function(result) {
+}).catch(console.error);
+
+var vg_2 = "bubbleplot.vg.json";
+vegaEmbed("#bubbleplot", vg_2).then(function(result) {
+}).catch(console.error)
