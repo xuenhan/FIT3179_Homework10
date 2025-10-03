ba3c7e339bd3352aa7ad78a4f8ece99633e6e12c
xuenhan <xshe0033@student.monash.edu>
2025-10-03 14:08:04 +0800

    Updated name from bubbleplot to lineplot

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,6 +2,6 @@ var vg_1 = "mean_household_income.vg.json";
 vegaEmbed("#chloropleth_map", vg_1).then(function(result) {
 }).catch(console.error);
 
-var vg_2 = "bubbleplot.vg.json";
-vegaEmbed("#bubbleplot", vg_2).then(function(result) {
+var vg_2 = "lineplot.vg.json";
+vegaEmbed("#lineplot", vg_2).then(function(result) {
 }).catch(console.error)
